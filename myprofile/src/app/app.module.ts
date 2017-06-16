e7dc2ed28d22398284dd7844c711c90ba5df24ff
weiping huang <weipunghuang@weipings-MacBook-Pro.local>
2017-06-16 00:49:19 -0400

    Added routing, navbar and home page

--- myprofile/src/app/app.module.ts
+++ myprofile/src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppRoutingModule } from './app.routing.module';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
--- myprofile/src/app/app.routing.module.ts
+++ myprofile/src/app/app.routing.module.ts
@@ -1,12 +1,20 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+const appRoutes : Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'dashboard', component: DashboardComponent}
+  //{path: '', component}
+]
 
 @NgModule({
   declarations: [
   ],
-  imports: [
-  ],
+  imports: [RouterModule.forRoot(appRoutes)],
   providers: [],
-  bootstrap: []
+  bootstrap: [],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
